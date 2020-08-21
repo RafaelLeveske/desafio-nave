043bc02db45386d79d996ce98c658288c539bb81
Rafael Vieira <rafaelleveske@gmail.com>
2020-08-21 11:14:24 -0300

    feature/login-page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import logo from '../../assets/logo.svg';
+
 import './styles.css';
 
 function Login() {
   return (
-    <div>
-      <h1>Hello login</h1>
+    <div className="home-page container">
+      <fieldset>
+        <form className="login-form" action="#">
+          <div className="header-form">
+            <img src={logo} alt="Nave" />
+            <strong>nave.rs</strong>
+          </div>
+          <div className="input-form">
+            <span>E-mail</span>
+            <input type="email" placeholder="E-mail" />
+            <span>Senha</span>
+            <input type="password" placeholder="Senha" />
+            <button type="submit">Entrar</button>
+          </div>
+        </form>
+      </fieldset>
     </div>
   );
 }
