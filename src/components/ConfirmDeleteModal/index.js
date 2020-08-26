0cde1e7cd9e5bd4d23891c608c048c55f4269481
Rafael Vieira <rafaelleveske@gmail.com>
2020-08-26 10:26:32 -0300

    feature/delete-naver

--- src/components/ConfirmDeleteModal/index.js
+++ src/components/ConfirmDeleteModal/index.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
-
-import WarningDeleteModal from '../WarningDeleteModal';
+import React from 'react';
 
 import './styles.css';
 
 function ConfirmDeleteModal({
   id = 'confirm-delete-modal',
   onClose = () => {},
+  children,
 }) {
-  const [isModalVisible, setIsModalvisible] = useState(false);
-
   const handleOutsideClick = e => {
     if (e.target.id === id) onClose();
   };
@@ -23,26 +20,8 @@ function ConfirmDeleteModal({
             Tem certeza que deseja excluir esse Naver?
           </span>
         </div>
-        <div className="close-modal-button-delete">
-          <button
-            className="close-modal-cancel"
-            type="button"
-            onClick={onClose}
-          >
-            Cancelar
-          </button>
-          <button
-            className="close-modal-delete"
-            type="button"
-            onClick={() => setIsModalvisible(true)}
-          >
-            Excluir
-          </button>
-        </div>
+        <div className="close-modal-button-delete">{children}</div>
       </div>
-      {isModalVisible ? (
-        <WarningDeleteModal onClose={() => setIsModalvisible(false)} />
-      ) : null}
     </div>
   );
 }
