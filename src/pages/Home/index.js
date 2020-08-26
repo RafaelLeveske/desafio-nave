44ccd01fc34559be7fc4cba66b9d87a9a71bd855
Rafael Vieira <rafaelleveske@gmail.com>
2020-08-26 09:55:31 -0300

    feature/home-list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 import ProfileModal from '../../components/ProfileModal';
 import ConfirmDeleteModal from '../../components/ConfirmDeleteModal';
 
-import avatar from '../../assets/avatar.svg';
 import trash from '../../assets/trash.svg';
 import edit from '../../assets/edit.svg';
 
@@ -14,6 +15,35 @@ import './styles.css';
 function Home() {
   const [isModalVisible, setIsModalvisible] = useState(false);
   const [isDeleteModalVisible, setIsDeleteModalvisible] = useState(false);
+  const [navers, setNavers] = useState([]);
+
+  const token = localStorage.getItem('@Navedex:token');
+
+  useEffect(() => {
+    api
+      .get('navers', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(response => {
+        setNavers(response.data);
+      });
+  }, [token]);
+
+  // async function handleDeleteNaver(id) {
+  //   try {
+  //     await api.delete(`navers/${id}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+
+  //     setNavers(navers.filter(naver => naver.id !== id));
+  //   } catch (err) {
+  //     alert('Erro ao deletar o Naver, tente novamente');
+  //   }
+  // }
 
   return (
     <div className="home-page container">
@@ -27,113 +57,36 @@ function Home() {
         </div>
         <section className="middle-content">
           <ul className="naver-profile">
-            <li>
-              <button
-                className="modal-button"
-                type="button"
-                onClick={() => setIsModalvisible(true)}
-              >
-                <img src={avatar} alt="Avatar" className="avatar" />
-              </button>
-              {isModalVisible ? (
-                <ProfileModal onClose={() => setIsModalvisible(false)} />
-              ) : null}
-              <strong>Juliano Reis</strong>
-              <span>Front-end Developer</span>
-              <div className="icons">
-                <button
-                  type="button"
-                  onClick={() => setIsDeleteModalvisible(true)}
-                >
-                  <img src={trash} alt="Trash" />
-                </button>
+            {navers.map(naver => {
+              return (
+                <li key={naver.id}>
+                  <button
+                    className="modal-button"
+                    type="button"
+                    onClick={() => setIsModalvisible(true)}
+                  >
+                    <img src={naver.url} alt="Avatar" className="avatar" />
+                  </button>
+                  {isModalVisible ? (
+                    <ProfileModal onClose={() => setIsModalvisible(false)} />
+                  ) : null}
+                  <strong>{naver.name}</strong>
+                  <span>{naver.job_role}</span>
+                  <div className="icons">
+                    <button
+                      type="button"
+                      onClick={() => setIsDeleteModalvisible(true)}
+                    >
+                      <img src={trash} alt="Trash" />
+                    </button>
 
-                <Link to="/update">
-                  <img src={edit} alt="Edit" />
-                </Link>
-              </div>
-            </li>
-
-            <li>
-              <button
-                className="modal-button"
-                type="button"
-                onClick={() => setIsModalvisible(true)}
-              >
-                <img src={avatar} alt="Avatar" className="avatar" />
-              </button>
-              {isModalVisible ? (
-                <ProfileModal onClose={() => setIsModalvisible(false)} />
-              ) : null}
-              <strong>Juliano Reis</strong>
-              <span>Front-end Developer</span>
-              <div className="icons">
-                <button
-                  type="button"
-                  onClick={() => setIsDeleteModalvisible(true)}
-                >
-                  <img src={trash} alt="Trash" />
-                </button>
-
-                <Link to="/update">
-                  <img src={edit} alt="Edit" />
-                </Link>
-              </div>
-            </li>
-
-            <li>
-              <button
-                className="modal-button"
-                type="button"
-                onClick={() => setIsModalvisible(true)}
-              >
-                <img src={avatar} alt="Avatar" className="avatar" />
-              </button>
-              {isModalVisible ? (
-                <ProfileModal onClose={() => setIsModalvisible(false)} />
-              ) : null}
-              <strong>Juliano Reis</strong>
-              <span>Front-end Developer</span>
-              <div className="icons">
-                <button
-                  type="button"
-                  onClick={() => setIsDeleteModalvisible(true)}
-                >
-                  <img src={trash} alt="Trash" />
-                </button>
-
-                <Link to="/update">
-                  <img src={edit} alt="Edit" />
-                </Link>
-              </div>
-            </li>
-
-            <li>
-              <button
-                className="modal-button"
-                type="button"
-                onClick={() => setIsModalvisible(true)}
-              >
-                <img src={avatar} alt="Avatar" className="avatar" />
-              </button>
-              {isModalVisible ? (
-                <ProfileModal onClose={() => setIsModalvisible(false)} />
-              ) : null}
-              <strong>Juliano Reis</strong>
-              <span>Front-end Developer</span>
-              <div className="icons">
-                <button
-                  type="button"
-                  onClick={() => setIsDeleteModalvisible(true)}
-                >
-                  <img src={trash} alt="Trash" />
-                </button>
-
-                <Link to="/update">
-                  <img src={edit} alt="Edit" />
-                </Link>
-              </div>
-            </li>
+                    <Link to="/update">
+                      <img src={edit} alt="Edit" />
+                    </Link>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
           {isDeleteModalVisible ? (
             <ConfirmDeleteModal
